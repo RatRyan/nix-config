5fbb40bd1a44548535f1aba07d9d2f8b5eef2bbc
RatRyan <ryantratajczak@gmail.com>
2024-03-02 20:01:51 -0700

    update

--- configuration.nix
+++ configuration.nix
@@ -1,14 +1,7 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
   # Bootloader.
   boot.loader = {
@@ -23,6 +16,7 @@
     };
   };
 
+  # I'm a coward :P
   services.envfs.enable = true;
 
   # Enable OpenGL
