c9512683e8f94b5c47014f43a9106049fc942a72
RatRyan <ryantratajczak@gmail.com>
2024-03-03 21:16:33 -0700

    feat: create keyboard.nix

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -45,16 +45,16 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
   };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
