861aa268ddd83cb5501f591932ebcd75cbea337d
RatRyan <ryantratajczak@gmail.com>
2024-04-09 20:15:09 -0600

    update

--- home/desktop/hyprland/gtk.nix
+++ home/desktop/hyprland/gtk.nix
@@ -1,28 +1,9 @@
 { config, pkgs, ... }: {
-  dconf.settings = {
-      "org/gnome/desktop/background" = {
-        picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
-      };
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-      };
-    };
-
-    gtk = {
-      enable = true;
-      theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome.gnome-themes-extra;
-      };
-    };
-
-    # Wayland, X, etc. support for session vars
-    systemd.user.sessionVariables = config.home-manager.users.ryan.home.sessionVariables;
-  };
-
-  qt = {
+  gtk = {
     enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome.gnome-themes-extra;
+    };
   };
 }
