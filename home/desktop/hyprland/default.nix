d059786cc3fbebe6d6327f8909410740d238b3a2
RatRyan <ryantratajczak@gmail.com>
2024-04-09 20:51:31 -0600

    update

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -18,6 +18,7 @@
   home.packages = with pkgs; [
     networkmanagerapplet
     blueman
+    hyprpaper
     gnome.nautilus
   ];
 
--- home/desktop/hyprland/gtk.nix
+++ home/desktop/hyprland/gtk.nix
@@ -6,4 +6,13 @@
       package = pkgs.gnome.gnome-themes-extra;
     };
   };
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Tokyonight-Dark-B-LB";
+    };
+  };
 }
