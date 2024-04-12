88d4876fc4f52897aac2f165b0c8b9d991d20d32
RatRyan <ryantratajczak@gmail.com>
2024-04-12 14:38:26 -0600

    update

--- hosts/common/plasma.nix
+++ hosts/common/plasma.nix
@@ -1,5 +1,5 @@
 {
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 }
