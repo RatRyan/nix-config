28042a9f511acdc5a98790a91698e5d68695d187
RatRyan <ryantratajczak@gmail.com>
2024-04-19 11:46:21 -0600

    update

--- hosts/byregot/default.nix
+++ hosts/byregot/default.nix
@@ -1,14 +1,23 @@
 { inputs, ... }: {
   imports = [
     inputs.hardware.nixosModules.common-cpu-amd
+    inputs.hardware.nixosModules.common-gpu-nvidia-nonprime
     inputs.hardware.nixosModules.common-pc-ssd
     ./hardware-configuration.nix
 
     ../common
-    ../common/optional/nvidia.nix
   ];
 
   networking.hostName = "byregot";
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+  };
 
   system.stateVersion = "23.11";
 }
