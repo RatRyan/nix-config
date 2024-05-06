c61e73d28a329f327ea7a4214346af609ff3d079
RatRyan <ryantratajczak@gmail.com>
2024-05-05 23:16:43 -0600

    update

--- hosts/common/app/plasma.nix
+++ hosts/common/app/plasma.nix
@@ -1,6 +1,22 @@
-{
+{ pkgs, ... }: {
   services.xserver.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   xdg.portal.enable = true;
+
+  services.xserver = {
+    desktopManager.gnome.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    grim
+    slurp
+    mako
+  ];
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
 }
+
