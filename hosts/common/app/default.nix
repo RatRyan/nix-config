444c678e37bc0bda0f9b5870a32462e09ddb2b75
RatRyan <ryantratajczak@gmail.com>
2024-05-15 12:44:06 -0600

    update

--- hosts/common/app/default.nix
+++ hosts/common/app/default.nix
@@ -4,17 +4,16 @@
 
   services = {
     xserver.enable = true;
-    gnome.gnome-keyring.enable = true;
-    displayManager.sddm.wayland.enable = true;
+    xserver.displayManager.sddm.wayland.enable = true;
     desktopManager.plasma6.enable = true;
+    gnome.gnome-keyring.enable = true;
   };
   xdg.portal.enable = true;
 
   environment.systemPackages = with pkgs; [
-    grim # screenshot functionality
-    slurp # screenshot functionality
-    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
-    mako # notification system developed by swaywm maintainer
+    grim
+    slurp
+    mako
   ];
 
   # enable sway window manager
