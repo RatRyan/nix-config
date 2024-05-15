abe490dc5db9749acb1eacaf6316ea09e7987994
RatRyan <ryantratajczak@gmail.com>
2024-05-15 12:52:58 -0600

    update

--- hosts/common/app/default.nix
+++ hosts/common/app/default.nix
@@ -4,21 +4,8 @@
 
   services = {
     xserver.enable = true;
-    xserver.displayManager.sddm.wayland.enable = true;
+    displayManager.sddm.wayland.enable = true;
     desktopManager.plasma6.enable = true;
-    gnome.gnome-keyring.enable = true;
   };
   xdg.portal.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    grim
-    slurp
-    mako
-  ];
-
-  # enable sway window manager
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-  };
 }
