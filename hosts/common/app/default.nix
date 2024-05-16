dfe1ff2b3a9c7cd9cedd5c11b30f7d2df1f9b4fe
RatRyan <ryantratajczak@gmail.com>
2024-05-16 10:41:44 -0600

    update

--- hosts/common/app/default.nix
+++ hosts/common/app/default.nix
@@ -14,7 +14,6 @@
     slurp # screenshot functionality
     wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
     mako # notification system developed by swaywm maintainer
-    swayfx
   ];
 
   # Enable the gnome-keyring secrets vault. 
