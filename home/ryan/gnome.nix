7486958fcf736ba53c9b672771281be3bf82a451
RatRyan <ryantratajczak@gmail.com>
2024-05-20 15:24:48 -0600

    update

--- home/ryan/gnome.nix
+++ home/ryan/gnome.nix
@@ -1,11 +1,20 @@
 {
   dconf.settings = {
+    # General settings
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
       text-scaling-factor = 1.50;
     };
-    "org/gnome/mutter" = {
+    "org/gnome/mutter/" = {
       experimental-features = [ "scale-monitor-framebuffer" ];
+    };
+
+    # Keybinds
+    "org/gnome/desktop/wm/keybindings" = {
+      move-to-workspace-1 = [ "<Shift><Super>1" ];
+      move-to-workspace-2 = [ "<Shift><Super>2" ];
+      move-to-workspace-3 = [ "<Shift><Super>3" ];
+      move-to-workspace-4 = [ "<Shift><Super>4" ];
     };
   };
 }
