a760264cc16963067c7c99d0337da297699ac731
RatRyan <ryantratajczak@gmail.com>
2024-05-20 15:17:24 -0600

    update

--- home/ryan/gnome.nix
+++ home/ryan/gnome.nix
@@ -2,7 +2,7 @@
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
-      text-scaling-factor = 1.37;
+      text-scaling-factor = 1.50;
     };
     "org/gnome/mutter" = {
       experimental-features = [ "scale-monitor-framebuffer" ];
