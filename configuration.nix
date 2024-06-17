ae6727dfd9bece210de57fcd5e221b8ce6b6dbaa
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-06-17 02:13:08 -0600

    update host

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }: {
-  imports = [
-    ./hardware/thinkpad.nix
-  ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
   boot.loader.efi.canTouchEfiVariables = true;
@@ -11,13 +9,6 @@
     efiSupport = true;
     useOSProber = true;
   };
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -68,8 +59,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
--- flake.nix
+++ flake.nix
@@ -1,16 +1,20 @@
 {
-  description = "Flake for my systems";
+  description = ":)";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-      ];
+    nixosConfigurations = {
+      ifrit = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware/thinkpad.nix
+          ./configuration.nix
+          { networking.hostName = "ifrit"; }
+        ];
+      };
     };
   };
 }
