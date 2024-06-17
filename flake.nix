9863c6a2ae4d21deff6eda7b35295b6a1a3d7fbc
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-06-17 01:43:45 -0600

    format

--- flake.nix
+++ flake.nix
@@ -1,15 +1,16 @@
 {
-  description = "A very basic flake";
+  description = "Flake for my systems";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
   };
 }
