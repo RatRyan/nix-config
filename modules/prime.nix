8b72bb2f86812953cede50298a3de0f7afe89b9b
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-06-19 17:11:06 -0600

    change prime to sync

--- modules/prime.nix
+++ modules/prime.nix
@@ -1,7 +1,6 @@
 {
   hardware.nvidia.prime = {
-    offload.enable = true;
-    offload.enableOffloadCmd = true;
+    sync.enable = true;
 
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
