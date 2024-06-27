a2e4a9aff7613747b5797e16a9d2beb13f372e57
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-06-26 23:48:05 -0600

    format some files

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -1,4 +1,5 @@
-{ config, ... }: {
+{ config, ... }:
+{
   # Nvidia GPU and Prime
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia.modesetting.enable = true;
--- system/sway.nix
+++ system/sway.nix
@@ -3,7 +3,6 @@
   environment.systemPackages = with pkgs; [
     grim # screenshot functionality
     slurp # screenshot functionality
-    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
     mako # notification system developed by swaywm maintainer
   ];
 
