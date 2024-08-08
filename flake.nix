273fb0766812253277ea90442272efc8a6908b73
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-07 23:31:10 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       {
         system = "x86_64-linux";
         hardware = "thinkpad";
-        sssssstateVersion = "24.05";
+        ssssstateVersion = "24.05";
         extraModules = [
           ./modules/nixos/thinkpad.nix
           ./modules/nixos/keyd.nix
