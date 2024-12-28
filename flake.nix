a6b20fb2d1ab173972f1dd0d264670f9cdbdbd1a
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-12-27 23:51:02 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outs = { nixpkgs, ... }@inputs: 
+  ous = { nixpkgs, ... }@inputs: 
   let
     mkSystem = import ./mksystem.nix { inherit inputs nixpkgs; };
   in
